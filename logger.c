0ba54a8ecf3552903e1aa5731ad0de4a71c9aa30
Helytre <matthieu.tironneau@efrei.net>
2025-10-20 18:40:44 +0200

    creation of logger files

---
+++ logger.c
@@ -0,0 +1,16 @@
+//
+// Created by Helytre on 10/20/25.
+//
+
+#include "logger.h"
+
+void logger(int LOGGING){
+    if(!LOGGING) return;
+    time_t sec;
+    time(&sec);
+    int min = sec//60 + 1970
+    int month = sec//
+
+    char* name = 
+    FILE* logs = fopen(name,"w")
+}
---
+++ logger.h
@@ -0,0 +1,19 @@
+//
+// Created by Helytre on 10/20/25.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef SSDP_2_PROJET_LOGGER_H
+#define SSDP_2_PROJET_LOGGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int LOGGING = 1;
+const int SAVING = 1;
+
+void logger(int LOGGING);
+
+#endif //SSDP_2_PROJET_LOGGER_H
