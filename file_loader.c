fdf15b8c5d18c198952f7c0980f4157d08541db2
Helytre <matthieu.tironneau@efrei.net>
2025-11-07 11:28:09 +0100

    TODO solved, import file is now working

--- file_loader.c
+++ file_loader.c
@@ -6,20 +6,19 @@
 
 t_adj_list* list_import(const char* file_name){
     FILE *adj_file = fopen(file_name, "r");
-    fread(adj_file, sizeof(int), 1, adj_file);
     int size , node, index_to;
     float weight;
     if (adj_file==NULL) {
         printf("Could not open file for reading");
         exit(EXIT_FAILURE);
     }
-    if (fscanf(adj_file, "%d", &size) != 1) { //TODO pb fscanf
+    if (fscanf(adj_file, "%d", &size) != 1) {
         printf("Could not read number of vertices");
         exit(EXIT_FAILURE);
     }
     t_adj_list* adj_list = create_empty_adj_list(size);
     while (fscanf(adj_file, "%d %d %f", &node, &index_to, &weight) == 3){
-        add_cell_to_adj_list(adj_list, node, index_to, weight);
+        add_cell_to_adj_list(adj_list, node-1, index_to, weight);
     }
     fclose(adj_file);
     return adj_list;
