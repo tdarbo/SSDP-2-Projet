0feccfa38d2ce5e0c5b340f221573d6353814037
Helytre <matthieu.tironneau@efrei.net>
2025-11-07 11:09:07 +0100

    creation of list import function

--- file_loader.c
+++ file_loader.c
@@ -4,6 +4,27 @@
 
 #include "file_loader.h"
 
+t_adj_list* list_import(const char* file_name){
+    FILE *adj_file = fopen(file_name, "r");
+    fread(adj_file, sizeof(int), 1, adj_file);
+    int size , node, index_to;
+    float weight;
+    if (adj_file==NULL) {
+        printf("Could not open file for reading");
+        exit(EXIT_FAILURE);
+    }
+    if (fscanf(adj_file, "%d", &size) != 1) { //TODO pb fscanf
+        printf("Could not read number of vertices");
+        exit(EXIT_FAILURE);
+    }
+    t_adj_list* adj_list = create_empty_adj_list(size);
+    while (fscanf(adj_file, "%d %d %f", &node, &index_to, &weight) == 3){
+        add_cell_to_adj_list(adj_list, node, index_to, weight);
+    }
+    fclose(adj_file);
+    return adj_list;
+}
+
 void list_export(t_adj_list graph){
     FILE *export_file = fopen("D:/0_Code/C++/SSDP-2-Projet/export/list.txt", "w");
     if (export_file != NULL){
--- file_loader.h
+++ file_loader.h
@@ -5,8 +5,10 @@
 #ifndef SSDP_2_PROJET_FILELOADER_H
 #define SSDP_2_PROJET_FILELOADER_H
 
+#include <stdlib.h>
 #include "adjacency_list.h"
 
+t_adj_list* list_import(const char*);
 void list_export(t_adj_list);
 
 #endif //SSDP_2_PROJET_FILELOADER_H
