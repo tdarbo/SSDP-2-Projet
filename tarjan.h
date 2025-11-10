2d7dfde9cc1f880cff845038bb613602808a0cf3
Helytre <matthieu.tironneau@efrei.net>
2025-11-10 17:52:59 +0100

    creation pile

--- tarjan.h
+++ tarjan.h
@@ -7,7 +7,16 @@
 
 #include "adjacency_list.h"
 #include "partition.h"
+#include "vertex.h"
+struct s_pile{
+    int* pile;
+    int size;
+}typedef t_pile;
 
-t_
+t_pile create_pile();
+void destroy_pile(t_pile*);
+int pop(t_pile*);
+void push(t_pile*, int);
+void delete(t_pile*);
 
 #endif //SSDP_2_PROJET_TARJAN_H
