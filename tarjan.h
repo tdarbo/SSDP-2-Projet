cc66b0a27701725dfff8e8e1460e333a84996e2c
thomas <thomas.darbo@efrei.net>
2025-11-10 17:23:31 +0100

    update types

---
+++ tarjan.h
@@ -0,0 +1,13 @@
+//
+// Created by thomas on 11/10/25.
+//
+
+#ifndef SSDP_2_PROJET_TARJAN_H
+#define SSDP_2_PROJET_TARJAN_H
+
+#include "adjacency_list.h"
+#include "partition.h"
+
+t_
+
+#endif //SSDP_2_PROJET_TARJAN_H
